93b1b35e57892cb3428954941335b86c9b34ab99
liuxiaoxiang <38720034@qq.com>
2018-02-05 07:03:02 +0800

    2.5

---
+++ zqxt_views/calc/views.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'home.html')
+
+def add(request):
+    a = request.GET['a']
+    b = request.GET['b']
+    c = int(a) + int(b)
+    return HttpResponse(str(c))
+
+def add2(request, a, b):
+    c = int(a) + int(b)
+    return HttpResponse(str(c))
+
+
+
+
